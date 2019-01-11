72f7445052a9c6d79b3a2efd94a82840a325f1bf
shussain03 <shussain3@spartaglobal.com>
2019-01-11 11:44:56 +0000

    java practice

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// alert ("Hello me!");
+//console.log ("Hi there")
+//var myNumber1 = -50;
+//var myNumber2 = 90;
+//console.log(myNumber1 + myNumber2);
+//myNumber = "Good Morning";
+
+//console.log(("+myNumber +")+("+myNumber1))
+//var myNumber = -90;
+//var myNumber1 = 500;
+
