28b0245b2cfdc8ffb9fa51692befcde70f1390e2
shussain03 <shussain3@spartaglobal.com>
2019-01-11 15:06:03 +0000

    javatest

--- js/main.js
+++ js/main.js
@@ -9,3 +9,93 @@
 //var myNumber = -90;
 //var myNumber1 = 500;
 
+
+// var myString = "hello";
+// console.log(myString + "jack");
+// console.log(myString.length);
+
+// var myNumber3 = 90;
+// var myNumber3 = myNumber3 + 1;
+// console.log(myNumber3 + 1);
+// console.log(myNumber3)
+
+
+// var newString = "Hello Nadi's sister!";
+// console.log(newString.indexOf("e"))
+
+// var myArr = ["H", "e", "l", "l", "o"]
+// console.log(myArr)
+
+// var myMoney = 1000000;
+// var housePrice = 800000;
+
+// if (myMoney >= housePrice) {
+//     console.log("You can afford it");
+//     console.log("You are Lucky!")
+// } else if (myMoney === 0) {
+//     console.log("You are broke");
+// } else {
+//     console.log("Not a chance");
+// }
+// console.log
+
+//integer
+var int1 = 1;
+var int2 = 2;
+console.log(int1 + int2);
+
+//float
+//var float1 = 2.3;
+//var float2 = 12.67;
+//console.log(float1 - float2);
+//console.log(Math.PI);
+
+
+//string
+var str1 = "This is a string 1";
+var str2 = 'This is a string 2';
+var str3 = `This is a string 3`;
+
+var conCat = str1 + "Hello World"; //adding 2 or more string 
+var interpolated = `String 3;${str3}` //use this syntax ${...}
+
+//console.log(typeof interpolated);
+//console.log(typeof interpolated.length);
+//console.log(interpolated.length);
+//console.log(interpolated);
+
+//Boolean
+var bol1 = true;
+var bol2 = false;
+
+//console.log(bol1 + bol2);
+//console.log(bol1 + bol2);
+//console.log(bol2 - bol2);
+
+
+//===FALSY===
+//""
+//0
+//undefined
+//false
+//null
+//NaN
+
+//===TRUTHY===
+//Everything else
+//1 or -1
+//true
+//"string"
+//[]
+//{}
+
+
+//if (undefined) {} else {} else {
+//  console.log("truth")
+//}
+//console.log("falsy")
+//}
+
+
+if (answer) {
+    console.log(`welcome, ${answer}`) console.log("please provide your name"))
